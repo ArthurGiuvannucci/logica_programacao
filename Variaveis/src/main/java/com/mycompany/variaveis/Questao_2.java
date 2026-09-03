81e7963959cf180a00f6cf93fdf50aadf0ff5992
Arthur Giuvannucci <arthurgyuvannuccy@gmail.com>
2026-09-03 18:04:45 -0300

    Atividades

---
+++ Variaveis/src/main/java/com/mycompany/variaveis/Questao_2.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.variaveis;
+
+/**
+ *
+ * @author ArthurGiuvannucci
+ */
+import java.util.Scanner;
+public class Questao_2 {
+    public static void main(String[] args) {
+        // Variável
+        int idade;
+        // criar entrada 
+        Scanner entradaNumero= new Scanner  (System.in);
+       
+        //solicitacao
+        System.out.println("Digite sua idade: ");
+        idade=entradaNumero.nextInt ();
+        // impressao
+        System.out.println("Digite sua idade "+idade+" anos");
+        
+        
+        
+        
+        
+    }
+}
